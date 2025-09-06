60d24c358e49756fa7cca164fd03d3ef97570e54
san-backend <67942847+cloudhat@users.noreply.github.com>
2025-09-06 14:14:18 +0900

    init

---
+++ src/main/java/test/newswebsocket/NewsWebsocketApplication.java
@@ -0,0 +1,13 @@
+package test.newswebsocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsWebsocketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewsWebsocketApplication.class, args);
+    }
+
+}
